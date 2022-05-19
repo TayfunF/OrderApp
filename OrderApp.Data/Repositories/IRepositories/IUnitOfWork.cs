2ca5d74c864fbe93ab196db39418199eb4fcb646
TayfunF <firtinatayfun08@gmail.com>
2022-05-19 15:00:01 +0300

    IUnitOfWork Created

---
+++ OrderApp.Data/Repositories/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Data.Repositories.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IAppUserRepository AppUser { get; }
+        ICartRepository Cart { get; }
+        ICategoryRepository Category { get; }
+        IOrderDetailsRepository OrderDetails { get; }
+        IOrderProductRepository OrderProduct { get; }
+        IProductRepository Product { get; }
+
+        void Save();
+    }
+}
