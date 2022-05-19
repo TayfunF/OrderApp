e29b6637c6d78724fd931a1f5c5fa5f580825527
TayfunF <firtinatayfun08@gmail.com>
2022-05-19 15:14:35 +0300

    CategoryControllers Created

---
+++ OrderApp/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApp.Data.Repositories.IRepositories;
+using OrderApp.Models;
+
+namespace OrderApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CategoriesList = _unitOfWork.Category.GetAll(null);
+
+            return View(CategoriesList);
+        }
+    }
+}
