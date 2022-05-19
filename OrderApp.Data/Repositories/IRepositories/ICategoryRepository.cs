b609c51a7aff619167f6c0db113339d071588763
TayfunF <firtinatayfun08@gmail.com>
2022-05-19 14:51:27 +0300

    IRepositories Updated

--- OrderApp.Data/Repositories/IRepositories/ICategoryRepository.cs
+++ OrderApp.Data/Repositories/IRepositories/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace OrderApp.Data.Repositories.IRepositories
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IGenericRepository<Category>
     {
     }
 }
