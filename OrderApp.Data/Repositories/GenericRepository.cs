c002dd0f8c3c5d4a122d10fe9eba18f46dcdea35
TayfunF <firtinatayfun08@gmail.com>
2022-05-19 14:42:12 +0300

    Generic Repository Created

---
+++ OrderApp.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApp.Data.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> _dbset;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbset = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbset.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> Query = _dbset;
+
+            if (filter != null)
+            {
+                Query = Query.Where(filter);
+            }
+
+            //Ornek : "Product , Order" → Product İcin Ayri , Order İcin Ayri Ekleme Yap
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Query = Query.Include(item);
+                }
+            }
+
+            return Query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> Query = _dbset;
+
+            if (filter != null)
+            {
+                Query = Query.Where(filter);
+            }
+
+            //Ornek : "Product , Order" → Product İcin Ayri , Order İcin Ayri Ekleme Yap
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Query = Query.Include(item);
+                }
+            }
+
+            return Query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbset.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbset.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbset.Update(entity);
+        }
+    }
+}
