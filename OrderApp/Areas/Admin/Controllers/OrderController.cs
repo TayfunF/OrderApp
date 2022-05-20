30e63ec18d946db5e03dbc6a59693a947845293b
TayfunF <firtinatayfun08@gmail.com>
2022-05-20 19:31:06 +0300

    Order Controller Created

---
+++ OrderApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApp.Data.Repositories.IRepositories;
+
+namespace OrderApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
