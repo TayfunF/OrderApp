8eb986e5d85ef7ab749e732ec8547f59ad286f20
TayfunF <firtinatayfun08@gmail.com>
2022-05-20 21:04:25 +0300

    Areas→Add→Scaffolided Item → Identity→ Add

--- OrderApp/Program.cs
+++ OrderApp/Program.cs
@@ -5,6 +5,13 @@ using OrderApp.Data.Repositories;
 using OrderApp.Data.Repositories.IRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
